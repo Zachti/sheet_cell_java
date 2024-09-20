39b1292d1d554b49643559f01c2b464a432afd75
Zachti <114566720+Zachti@users.noreply.github.com>
2024-09-20 21:13:06 +0300

    fix(sheet-cell): row index bug fix

--- logic/src/position/Position.java
+++ logic/src/position/Position.java
@@ -2,32 +2,10 @@ package position;
 
 import position.interfaces.IPosition;
 
-import static common.utils.InputValidation.validateOrThrow;
-
 public record Position(int row, char column) implements IPosition {
 
     @Override
     public String toString() {
         return String.format("%s%s", column , row);
-    }
-
-    public static Position fromString(String s) {
-       validateOrThrow(
-                s,
-                str -> str != null && str.length() == 2,
-                str -> "Invalid input string for Position: " + str
-        );
-
-        return PositionFactory.create(Character.getNumericValue(s.charAt(1)), s.charAt(0));
-    }
-
-    public static String getColumnLabel(int colIndex) {
-        StringBuilder columnLabel = new StringBuilder();
-        while (colIndex > 0) {
-            colIndex--;
-            columnLabel.insert(0, (char) ('A' + (colIndex % 26)));
-            colIndex /= 26;
-        }
-        return columnLabel.toString();
     }
 }
--- logic/src/position/PositionFactory.java
+++ logic/src/position/PositionFactory.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Integer.parseInt;
+
 public class PositionFactory {
 
     private static final Map<String, Position> cachedPositions = new HashMap<>();
 
-    public static Position create(int row, char column) {
+    private static Position create(int row, char column) {
         String key = row + ":" + column;
 
         return cachedPositions.computeIfAbsent(key, value -> new Position(row, column));
@@ -16,7 +18,7 @@ public class PositionFactory {
 
     public static Position create(String pos) {
         char col = pos.charAt(0);
-        int row = Character.getNumericValue(pos.charAt(1));
+        int row = parseInt(pos.substring(1));
         return create(row, col);
     }
 
