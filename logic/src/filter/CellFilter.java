b31e9bb15cb386dbb8a6d54afb402868ad10dff0
Zachti <114566720+Zachti@users.noreply.github.com>
2024-09-21 11:56:16 +0300

    fix(sheet-cell): position label

--- logic/src/filter/CellFilter.java
+++ logic/src/filter/CellFilter.java
@@ -36,28 +36,20 @@ public class CellFilter implements IFilter {
 
     @Override
     public Map<IPosition, Cell> byMultiColumns(IRange range, Map<Character,  List<String>> selectedValues, boolean isAnd) {
-        return isAnd ? ByAndValues(range, selectedValues) : null; //ByOrValues(range, selectedValues);
+        return isAnd ? ByAndValues(range, selectedValues) : ByOrValues(range, selectedValues);
     }
 
     private Map<IPosition, Cell> ByAndValues(IRange range, Map<Character,  List<String>> selectedValues) {
-        List<Integer> rows = selectedValues.values().stream()
-                .flatMap(values -> ByValues(range, values).keySet().stream())
-                .map(IPosition::row)
-                .distinct()
+        List<Integer> rows = getRowsInFilter(range, selectedValues).stream()
                 .filter(row -> containsAtLeastOneFromEachCol(row, selectedValues)).toList();
         return rowsToCellMap(range, rows);
 
     }
 
-//    private Map<IPosition, Cell> ByOrValues(IRange range, Map<Character,  List<String>> selectedValues) {
-//            List<Integer> rows = selectedValues.values().stream()
-//                    .flatMap(values -> ByValues(range, values).keySet().stream())
-//                    .map(IPosition::row)
-//                    .distinct().toList();
-//            rows.stream()
-//                    .reduce((row1, row2) -> Stream.concat(row1, row2).distinct().collect(Collectors.toList()))
-//                    .orElse(Collections.emptyList());
-//    }
+    private Map<IPosition, Cell> ByOrValues(IRange range, Map<Character,  List<String>> selectedValues) {
+            List<Integer> rows = getRowsInFilter(range, selectedValues).stream().distinct().toList();
+            return rowsToCellMap(range, rows);
+    }
 
     private List<Cell> getCellsByRows(IRange range, List<Integer> rows) {
         return byRange(range).stream().filter(cell -> rows.contains(cell.getPosition().row())).toList();
@@ -83,5 +75,12 @@ public class CellFilter implements IFilter {
     private Map<IPosition, Cell> rowsToCellMap(IRange range, List<Integer> rows) {
         return getCellsByRows(range, rows).stream()
                 .collect(Collectors.toMap(Cell::getPosition, Function.identity()));
+    }
+
+    private List<Integer> getRowsInFilter(IRange range, Map<Character,  List<String>> selectedValues) {
+        return selectedValues.values().stream()
+                .flatMap(values -> ByValues(range, values).keySet().stream())
+                .map(IPosition::row)
+                .distinct().toList();
     }
 }
